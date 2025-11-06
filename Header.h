f4e935851558c2bdee86d244c8e1a4795aa62951
a-haton <awhthebear@gmail.com>
2025-11-05 21:14:01 -0800

    DFS finished + added project descriptions

--- Header.h
+++ Header.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include <iostream>
@@ -12,45 +13,51 @@ using namespace std;
 // makes identifying cities easier
 enum City
 {
-	Seattle = 1,
-	Chicago = 2,
-	Boston = 3,
-	SanFrancisco = 4,
-	Denver = 5,
-	NewYork = 6,
-	LosAngeles = 7,
-	KansasCity = 8,
-	Atlanta = 9,
-	Dallas = 10,
-	Houston = 11,
-	Miami = 12
+    Seattle = 1,
+    Chicago = 2,
+    Boston = 3,
+    SanFrancisco = 4,
+    Denver = 5,
+    NewYork = 6,
+    LosAngeles = 7,
+    KansasCity = 8,
+    Atlanta = 9,
+    Dallas = 10,
+    Houston = 11,
+    Miami = 12
 };
 
 // class to track the cities  in a Adjacency List Structure
 class Map
 {
 private:
-	//int Vertices = 12;
+    //int Vertices = 12;
 
-	// Vector to store data for cities and their distances
-	vector<vector<pair<int, int>>> adjList;
+    // Vector to store data for cities and their distances
+    vector<vector<pair<int, int>>> adjList;
 
 
 public:
 
-	// constructor
-	Map();
+    // constructor
+    Map();
 
-	// adds a city's edges too the vector stored in Map object
-	void AddCity(int nameCityOne, int nameCityTwo, int distance);
+    // adds a city's edges too the vector stored in Map object
+    void AddCity(int nameCityOne, int nameCityTwo, int distance);
 
-	// each city is stored in the vector as a number and will need decoded
-	// pass the number to decode here to recieve the cities name string
-	string CityDecoder(int toDecode);
+    // each city is stored in the vector as a number and will need decoded
+    // pass the number to decode here to recieve the cities name string
+    string CityDecoder(int toDecode);
 
-	// display all cities and their distances to each other
-	void DisplayMap();
+    // display all cities and their distances to each other
+    void DisplayMap();
 
+    // function to handle the recursive DFS traversal of a graph stored in an Adjacency List structure.
+    //void dfsRecursive(vector<bool>& visited, int currCity, vector<int>& result, vector<pair<int, int>>& discoveryEdges);
+    void dfsRecursive(vector<bool>& visited, int currCity, vector<int>& result, vector<pair<int, int>>& discoveryEdges);
+
+    // function to perform a DFS traversal
+    void DFS();
 };
 
 // function to perform a BFS traversal
--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,29 @@
+/*******************************************************************************
+ * Name: Aaron, Dakota, and Hailey
+ * Assignment: Assignment 9
+ * Description:
+ * 	This program implements graph using the Adjacency List and Adjacency Matrix
+ * 	structure, and performs a BFS and DFS traversal on those graphs.
+ *
+ * ****************************************************************************/
 #include "Header.h"
 
 int main()
 {
-	Map UnitedStates;
+    cout << "This program takes a graph of the United States with 12 cities\n"
+         << "highlighted and performs both a DFS and BFS traversal of the map.\n"
+         << "It then displays all of the edges' types per traversal and the\n"
+         << "final ordering per traversal.\n\n";
+    Map UnitedStates;
+    //UnitedStates.DisplayMap();
+    cout << "______________________________________________________\n";
+    cout << "\nPart A: DFS Traversal\n";
+    UnitedStates.DFS();
 
-	UnitedStates.DisplayMap();
+    cout << "\n______________________________________________________\n";
+    cout << "\nPart B: BFS Traversal\n";
+    BFS();
 
-	BFS();
 
-	return 0;
+    return 0;
 }
